7fe279eb3780da77ee68e6e9b6eb884965aab1b3
hugo-dc <jhugodc@gmail.com>
2020-10-06 11:38:42 -0600

    Print transactions

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 
 
 head = {"Content-type": "application/json"}
-RPC_ENDPOINT = 'http://52.165.165.44:8545'
+RPC_ENDPOINT = 'http://localhost:8545'
 
 
 # get block
@@ -19,10 +19,38 @@ def get_block(blocknum):
 
     response = requests.post(RPC_ENDPOINT, data=json.dumps(payload), headers= head)
 
+    #f = open('block_' + str(blocknum) + '.json', 'w')
+    #f.write(str(response.json()['result']))
+    #f.close()
     return response.json()['result']
 
-block = get_block(1)
+number = int(sys.argv[1])
 
-print(block)
+block = get_block(int(number))
+#print(json.dumps(block, indent=4, sort_keys=True))
 
+print("block number: ", block['number'])
+print("total transactions: ", len(block['transactions']))
+
+print(block['transactions'][0])
+
+for ix, tx in enumerate(block['transactions']):
+    print("transaction", ix, ":", tx['hash'])
+    if tx['to'] == None:
+        print("\tdeploy contract")
+    else:
+        print("\tgetting code for", tx['to'])
+        payload = {
+            'method': 'eth_getCode',
+            'params': [tx['to'], hex(number)],
+            'id': 1
+        }
+        response = requests.post(RPC_ENDPOINT, data=json.dumps(payload), headers=head)
+        try:
+            code = response.json()['result']
+            print('\tcode:', code)
+        except:
+            print("ERROR with eth_getCode, response:", response.json())
+            print(response.text)
+    
 
