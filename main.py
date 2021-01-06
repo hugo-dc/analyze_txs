4db3bf1b389975713f1e39c31141f53ada4964d3
Hugo <jhugodc@gmail.com>
2021-01-05 22:09:20 -0700

    Remove unused code

--- main.py
+++ main.py
@@ -14,40 +14,16 @@ tracer_script = ts_file.read()
 ts_file.close()
 
 
-def sha3_256(data):
-    s = hashlib.sha3_256()
-    s.update(data)
-    return s.digest()
-
-def chunkify(program):
-    chunks = []
-    pos = 0
-    this_chunk_start = 0
-    this_chunk_code_start = 0
-    while pos < len(program):
-        pos += (program[pos] - 0x5f) if (0x60 <= program[pos] <= 0x7f) else 1
-        if pos >= this_chunk_start + 32:
-            hash = sha3_256(
-                    program[this_chunk_start:this_chunk_start + 32] +
-                    this_chunk_code_start.to_bytes(32, 'big'))
-            chunks.append(hash)
-            this_chunk_start += 32
-            this_chunk_code_start = pos
-    return chunks
-
 def get_current_block():
     payload = {
         'method': 'eth_blockNumber',
         'params': [],
         'id': 1
     }
-
     response = requests.post(RPC_ENDPOINT, data=json.dumps(payload), headers= head)
-
     return int(response.json()['result'], 16)
 
 
-# get block
 def get_block(blocknum):
     payload = {
         'method': 'eth_getBlockByNumber',
@@ -56,7 +32,6 @@ def get_block(blocknum):
     }
 
     response = requests.post(RPC_ENDPOINT, data=json.dumps(payload), headers= head)
-
     f = open('data/block_' + str(blocknum) + '.json', 'w')
     f.write(str(response.json()['result']))
     f.close()
