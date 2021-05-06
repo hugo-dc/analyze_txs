635f5e1c2efee867eb26827f19a77fb7fcfd1d7a
Hugo <jhugodc@gmail.com>
2021-05-06 12:50:15 -0600

    Add rpc methods

---
+++ rpc.py
@@ -0,0 +1,40 @@
+import requests
+import json
+
+head = {"Content-type": "application/json"}
+RPC_LOCAL = 'http://localhost:8545'
+RPC_INFURA = open('./infura_data.txt', 'r').read().trim()
+
+RPC_ENDPOINT = RPC_LOCAL # Change to infura if needed
+
+ts_file = open('op_tracer.js', 'r')
+tracer_script = ts_file.read()
+ts_file.close()
+
+def get_block_by_number(blocknum):
+    payload = {
+        'method': 'eth_getBlockByNumber',
+        'params': [hex(blocknum), True],
+        'id': 1
+    }
+
+    response = requests.post(RPC_ENDPOINT, data=json.dumps(payload), headers= head)
+    return response.json()
+
+def get_transaction_receipt(tx_hash):
+    payload = {
+        'method': 'eth_getTransactionReceipt',
+        'params': [tx_hash],
+        'id': 1
+    }
+    response = requests.post(RPC_ENDPOINT, data=json.dumps(payload), headers = head)
+    return response.json()
+
+def trace_transaction(tx_hash, tracer):
+    payload = {
+        'method': 'debug_traceTransaction',
+        'params': [tx_hash, {'tracer': tracer}],
+        'id': 1
+    }
+    response = requests.post(RPC_ENDPOINT, data=json.dumps(payload), headers = head)
+    return response.json()
