a7fb2e83d7a4994b83fef88906dd31e99a98bd6a
Jose Hugo De la cruz Romero <jhugodc@gmail.com>
2021-05-18 22:20:57 -0600

    Calculating data

--- rpc.py
+++ rpc.py
@@ -3,15 +3,46 @@ import json
 
 head = {"Content-type": "application/json"}
 RPC_LOCAL = 'http://localhost:8545'
-RPC_INFURA = open('./infura_data.txt', 'r').read().trim()
+RPC_INFURA = open('./infura_data.txt', 'r').read().strip()
+RPC_MAZLAB = 'http://192.168.1.82:8545'
 
-RPC_ENDPOINT = RPC_LOCAL # Change to infura if needed
+RPC_ENDPOINT = RPC_LOCAL # Change to infura/local if needed
+
+CACHEDIR = './cache/'
 
 ts_file = open('op_tracer.js', 'r')
 tracer_script = ts_file.read()
 ts_file.close()
 
+def set_rpc_endpoint(ep):
+    global RPC_ENDPOINT
+    if ep == "INFURA":
+        RPC_ENDPOINT = RPC_INFURA 
+    elif ep == 'MAZLAB':
+        RPC_ENDPOINT = RPC_MAZLAB
+    else:
+        RPC_ENDPOINT = RPC_LOCAL
+
+def read_cache_file(fname):
+    cache_file = open(fname, 'r')
+    jsondata = cache_file.read().strip()
+    if len(jsondata) == 0:
+        jsondata = "{}"
+    result = json.loads(jsondata)
+    cache_file.close()
+    return result
+
+def write_cache_file(fname, data):
+    cache_file = open(fname, 'w')
+    cache_file.write(data)
+    cache_file.close()
+
 def get_block_by_number(blocknum):
+    blocks = read_cache_file(CACHEDIR + 'blocks.json')
+
+    if str(blocknum) in blocks.keys():
+        return blocks[str(blocknum)]
+
     payload = {
         'method': 'eth_getBlockByNumber',
         'params': [hex(blocknum), True],
@@ -19,6 +50,10 @@ def get_block_by_number(blocknum):
     }
 
     response = requests.post(RPC_ENDPOINT, data=json.dumps(payload), headers= head)
+
+    blocks[blocknum] = response.json()
+    write_cache_file(CACHEDIR + 'blocks.json', json.dumps(blocks, indent=4, sort_keys=False))
+
     return response.json()
 
 def get_transaction_receipt(tx_hash):
